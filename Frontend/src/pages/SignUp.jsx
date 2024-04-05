3b34632a27d6e3ce7ccd4df580a96956b88d413c
Mohammad Usman <uk351675@gmail.com>
2024-04-05 23:29:24 +0530

    signup functionality complete,fetch api

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,38 +1,84 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success == false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/signin");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  /////////////////////////////////////////////////
   return (
     <div className="flex flex-col justify-center items-center p-4 mx-auto">
       <span className="font-semibold text-black text-3xl m-3 p-2 mx-auto">
         Sign Up
       </span>
       <form
+        onSubmit={handleSubmit}
         action=""
         className="flex flex-col justify-center items-center gap-4 w-full text-slate-600 p-4"
       >
         <input
           type="text"
           name=""
-          id=""
+          id="username"
           placeholder="Username"
           className="p-3 rounded-md w-1/3 focus:outline-none  shadow-sm"
+          onChange={handleChange}
         />
         <input
           type="text"
           name=""
-          id=""
+          id="email"
           placeholder="Email"
           className="p-3 rounded-md w-1/3 focus:outline-none  shadow-sm"
+          onChange={handleChange}
         />
         <input
           type="password"
           name=""
-          id=""
+          id="password"
           placeholder="Password"
           className="p-3 rounded-md w-1/3 focus:outline-none shadow-sm"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 hover:opacity-95 disabled:opacity-80 p-3 w-1/3 rounded-lg text-white shadow-sm">
-          SIGN UP
+        <button
+          disabled={loading}
+          className="bg-slate-700 hover:opacity-95 disabled:opacity-80 p-3 w-1/3 rounded-lg text-white shadow-sm"
+        >
+          {loading ? "Loading..." : "SIGN UP"}
         </button>
         <button className="bg-red-700 hover:opacity-95 disabled:opacity-80 p-3 w-1/3 rounded-lg text-white shadow-sm">
           CONTINUE WITH GOOGLE
@@ -44,6 +90,7 @@ function SignUp() {
           Sign in
         </Link>
       </div>
+      {error && <p className="text-red-700 mt-6">{error}</p>}
     </div>
   );
 }
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //yahan par proxy add kar rahe he baar baar http://localhost na likhna pade isliye
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
